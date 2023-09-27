eb236328fc1f8e48a1ed8b180d8f80c2f7e7ae15
Illia Beskrovnyi <b.ilya.work@gmail.com>
2023-09-27 20:24:28 +0300

    Implemented LinkedQueue module

--- linked-queue/src/main/java/com/bobocode/LinkedQueue.java
+++ linked-queue/src/main/java/com/bobocode/LinkedQueue.java
@@ -1,5 +1,7 @@
 package com.bobocode;
 
+import javax.annotation.Nonnull;
+
 /**
  * {@link LinkedQueue} implements FIFO {@link Queue}, using singly linked nodes. Nodes are stores in instances of nested
  * class Node. In order to perform operations {@link LinkedQueue#add(Object)} and {@link LinkedQueue#poll()}
@@ -8,15 +10,40 @@ package com.bobocode;
  * @param <T> a generic parameter
  */
 public class LinkedQueue<T> implements Queue<T> {
+    static class Node<T> {
+
+        T value;
+        Node<T> next = null;
+        Node<T> prev = null;
+
+        public Node(T value) {
+            this.value = value;
+        }
+    }
+
+    private Node<T> head;
+    private Node<T> tail;
+    private int size;
 
     /**
      * Adds an element to the end of the queue.
      *
      * @param element the element to add
      */
-    public void add(T element) {
-        throw new UnsupportedOperationException("This method is not implemented yet"); // todo: implement this method
+    public void add(@Nonnull  T element) {
+
+        var newNode = new Node<>(element);
+        if (tail == null) {
+            tail = newNode;
+            head = newNode;
+        } else {
+            newNode.next = tail;
+            tail.prev = newNode;
+            tail = newNode;
+        }
+        size++;
     }
+
 
     /**
      * Retrieves and removes queue head.
@@ -24,7 +51,23 @@ public class LinkedQueue<T> implements Queue<T> {
      * @return an element that was retrieved from the head or null if queue is empty
      */
     public T poll() {
-        throw new UnsupportedOperationException("This method is not implemented yet"); // todo: implement this method
+
+        if (isEmpty()) {
+            return null;
+        }
+
+        var temp = head.value;
+        var oneLeft = head == tail;
+        if (oneLeft) {
+            head = null;
+            tail = null;
+        } else {
+            head.prev.next = null;
+            head = head.prev;
+        }
+        size--;
+
+        return temp;
     }
 
     /**
@@ -33,7 +76,7 @@ public class LinkedQueue<T> implements Queue<T> {
      * @return an integer value that is a size of queue
      */
     public int size() {
-        throw new UnsupportedOperationException("This method is not implemented yet"); // todo: implement this method
+        return size;
     }
 
     /**
@@ -42,6 +85,6 @@ public class LinkedQueue<T> implements Queue<T> {
      * @return {@code true} if the queue is empty, returns {@code false} if it's not
      */
     public boolean isEmpty() {
-        throw new UnsupportedOperationException("This method is not implemented yet"); // todo: implement this method
+        return size == 0;
     }
 }
